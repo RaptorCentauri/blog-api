272a74299d24305d89b1ac08050d964dfed6c936
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-10-29 06:45:24 +0000

    file to hold config variables

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const config = {
 	port: process.env.PORT || 8080,
-	db: process.env.DB_URL || 'mongodb://127.0.0.1:27017/'
+	db: process.env.DB_URL || 'mongodb://127.0.0.1:27017'
 }
 
 export default config;
