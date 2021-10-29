f8867254d85a32ffae98cd5557632061b342e525
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-10-29 06:43:48 +0000

    create connection to mongodb

---
+++ config/config.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+	port: process.env.PORT || 8080,
+	db: process.env.DB_URL || 'mongodb://127.0.0.1:27017/'
+}
+
+export default config;
